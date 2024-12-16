261c47721eaa6afd202f89394e84d17da4b1d7f7
divyeshravi30 <divyeshravi30@gmail.com>
2024-12-16 12:15:17 +0530

    Update singleinherit.java

--- singleinherit.java
+++ singleinherit.java
@@ -1,12 +1,11 @@
-
 class singleinherit
-	{
+{
 		public static void main(String[] args)
 			{
 				int a = 10;
 				int b = 10;
 				int c = a+b;
-				system.out.print("Sum is : "+c);
+				System.out.print("Sum is : "+c);
 			}
 	}
 
